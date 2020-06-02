328d326ed0762edfa5f70fc548f99251ca78535c
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-06-02 02:38:36 +0100

    Launch ball when user clicks

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,14 +7,31 @@ public class Ball : MonoBehaviour {
     private Paddle paddle;
 
     private Vector2 _paddleToBallVector;
-
+    private Rigidbody2D _body;
+    private bool _laucnhed;
+    
     // Start is called before the first frame update
     void Start() {
+        _body = GetComponent<Rigidbody2D>();
         _paddleToBallVector = transform.position - paddle.transform.position;
     }
 
     // Update is called once per frame
     void Update() {
+        if (!_laucnhed) {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick() {
+        if (Input.GetMouseButtonDown(0)) {
+            _laucnhed = true;
+            _body.velocity = new Vector2(2f, 15f);
+        }
+    }
+
+    private void LockBallToPaddle() {
         Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = _paddleToBallVector + paddlePosition;
     }
