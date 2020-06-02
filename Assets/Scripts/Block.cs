3e7090933ecd210298a02ee0e4c4de54ff2dfce9
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-06-02 23:21:39 +0100

    Allow life for blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
 public class Block : MonoBehaviour {
@@ -8,24 +9,54 @@ public class Block : MonoBehaviour {
     [SerializeField]
     private AudioClip breakingSound;
 
+    [SerializeField]
+    private GameObject blockParticlesVfx;
+
+    [SerializeField]
+    private int maxHits;
+
+    [SerializeField]
+    private int hitsReceived;
+
     // Cached reference
     private Level _level;
     private GameSession _session;
 
     private void Start() {
-        _level = FindObjectOfType<Level>();
-        _level.CountBreakableBlocks();
+        CountBreakableBlocks();
         _session = FindObjectOfType<GameSession>();
     }
 
+    private void CountBreakableBlocks() {
+        _level = FindObjectOfType<Level>();
+        if (CompareTag("Breakable")) {
+            _level.CountBreakableBlocks();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
-        DestroyBlock();
+        if (CompareTag("Breakable")) {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit() {
+        hitsReceived++;
+        if (hitsReceived >= maxHits) {
+            DestroyBlock();
+        }
     }
 
     private void DestroyBlock() {
         _session.AddToScore();
         AudioSource.PlayClipAtPoint(breakingSound, Camera.main.transform.position);
         _level.BlockDestroyed();
+        TriggerSparklesVfx();
         Destroy(gameObject);
+    }
+
+    private void TriggerSparklesVfx() {
+        GameObject particles = Instantiate(blockParticlesVfx, transform.position, transform.rotation);
+        Destroy(particles, 2f);
     }
 }
