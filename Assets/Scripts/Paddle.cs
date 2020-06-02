df5815c8d0e61d3f33dc16baf19095d7963911bc
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-06-02 02:15:45 +0100

    Limit paddle movment

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField]
+    private float minX = 1f;
+
+    [SerializeField]
+    private float maxX = 15f;
+
+    [SerializeField]
+    private float screenWidthUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() {
+        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthUnits;
+        Vector2 paddlePosition =
+            new Vector2(transform.position.x, transform.position.y) {
+                x = Mathf.Clamp(mousePositionInUnits, minX, maxX)
+            };
+        transform.position = paddlePosition;
+    }
+}
