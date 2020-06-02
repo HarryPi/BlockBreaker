9a261efc3bb471e4c702bb21b12dc79bf6150c8d
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-06-02 02:29:50 +0100

    Make ball stick to paddle

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
-{
+public class Ball : MonoBehaviour {
+    [SerializeField]
+    private Paddle paddle;
+
+    private Vector2 _paddleToBallVector;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        _paddleToBallVector = transform.position - paddle.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = _paddleToBallVector + paddlePosition;
     }
 }
